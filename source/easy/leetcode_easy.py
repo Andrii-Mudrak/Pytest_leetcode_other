ba374264d9fdbc89b82ed85fee7ec7d90c6642f8
Andrii Mudrak <andriymudrak@gmail.com>
2024-08-13 13:04:03 +0300

    add two tests longest_common_prefix and merge_two_sorted_lists

--- source/easy/leetcode_easy.py
+++ source/easy/leetcode_easy.py
@@ -6,3 +6,43 @@ def lengthLastWord(s):
     return len(s.split()[-1])
 
 
+def longest_common_prefix(strs):
+    '''
+    https://leetcode.com/problems/longest-common-prefix/description/
+    Write a function to find the longest common prefix string amongst an array of strings.
+    If there is no common prefix, return an empty string "".
+    '''
+    shortest = strs[0]
+    temp = set()
+# find a least word
+    for item in strs[1:]:
+        if len(item) < len(shortest):
+            shortest = item
+    prf = shortest[0]
+# sort through the list based on the smallest word
+    for item in shortest:
+        idx =  shortest.index(item)
+        for content in strs:
+            temp.add(content[idx])
+        if len(prf) != len(temp):
+            if len(prf) != 1:
+                return prf[1:]
+                break
+            else:
+                return ""
+                break
+        prf += item
+
+
+def merged_two_sorted_lists(list1,list2):
+    '''
+    https://leetcode.com/problems/merge-two-sorted-lists/description/
+    Merge the two lists into one sorted list. The list should be made by
+    splicing together the nodes of the first two lists.
+    '''
+    result = []
+    result.extend(list1)
+    result.extend(list2)
+    result.sort()
+
+    return result
--- tests/test_easy.py
+++ tests/test_easy.py
@@ -7,17 +7,56 @@ import pytest
     ("   fly me   to   the moon  ", 4),
     ("luffy is still joyboy", 6)
 ])
-def test_length_Last_Word_positive(s, expected_result):
+def test_length_last_word_positive(s, expected_result):
     result = lengthLastWord(s)
     assert result == expected_result
 
 
-@pytest.mark.xfail(reason="negative")
+@pytest.mark.xfail(reason="Negative")
 @pytest.mark.parametrize("s, expected_result",[
     ("Hello World", 4),
     ("   fly me   to   the moon  ", 2),
     ("luffy is still joyboy", 1)
 ])
-def test_length_Last_Word_negative(s, expected_result):
+def test_length_last_word_negative(s, expected_result):
     result = lengthLastWord(s)
     assert result == expected_result
+
+
+@pytest.mark.wip
+@pytest.mark.parametrize("strs, expected_result",[
+        (["flower","flow","flight"], "fl"),
+        (["dog","racecar","car"], "")
+    ])
+def test_longest_common_prefix_positive(strs, expected_result):
+    result = longest_common_prefix(strs)
+    assert result == expected_result
+
+
+@pytest.mark.xfail(reason="Negative")
+@pytest.mark.parametrize("strs, expected_result",[
+        (["flower","flow","flight"], "fl"),
+        (["dog","racecar","car"], "")
+    ])
+def test_longest_common_prefix_negative(strs, expected_result):
+    result = longest_common_prefix(strs)
+    assert result != expected_result
+
+
+@pytest.mark.parametrize("list1, list2, expected_result",[
+    ([1,2,4], [1,3,4], [1,1,2,3,4,4]),
+    ([], [], [])
+])
+def test_merge_two_sorted_lists_positive(list1, list2, expected_result):
+    result = merged_two_sorted_lists(list1, list2)
+    assert result == expected_result
+
+
+@pytest.mark.xfail(reason="Negative")
+@pytest.mark.parametrize("list1, list2, expected_result",[
+    ([1,2,4], [1,3,4], [1,1,2,3,4,4]),
+    ([], [], [])
+])
+def test_merge_two_sorted_lists_negative(list1, list2, expected_result):
+    result = merged_two_sorted_lists(list1, list2)
+    assert result != expected_result
