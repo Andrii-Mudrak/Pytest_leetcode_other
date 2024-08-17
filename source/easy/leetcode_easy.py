b17d3a044ffcf2e569972c22a1e5aa99d187cab8
Andrii Mudrak <andriymudrak@gmail.com>
2024-08-17 22:27:07 +0300

    add some tests

--- source/easy/leetcode_easy.py
+++ source/easy/leetcode_easy.py
@@ -1,10 +1,9 @@
 def lengthLastWord(s):
-    '''
+    """
     https://leetcode.com/problems/length-of-last-word/description/
     Given a string s consisting of words and spaces, return the length of the last word in the string.
-    '''
+    """
     return len(s.split()[-1])
-
 
 def longest_common_prefix(strs):
     '''
@@ -33,7 +32,6 @@ def longest_common_prefix(strs):
                 break
         prf += item
 
-
 def merged_two_sorted_lists(list1,list2):
     '''
     https://leetcode.com/problems/merge-two-sorted-lists/description/
@@ -44,5 +42,58 @@ def merged_two_sorted_lists(list1,list2):
     result.extend(list1)
     result.extend(list2)
     result.sort()
-
     return result
+
+def search_insert(nums,target):
+    '''
+    https://leetcode.com/problems/search-insert-position/description/
+    Given a sorted array of distinct integers and a target value,
+    return the index if the target is found. If not, return the
+    index where it would be if it were inserted in order.
+    '''
+    new_nums = nums
+    number = int(len(new_nums)/2)
+    while number !=0:
+        if target <= new_nums[number-1]:
+            # move left
+            new_nums = new_nums[0:number]
+        else:
+            # move right
+            new_nums = new_nums[number:]
+        if number == 2:
+            result = None
+            if target > new_nums[1]:
+                result = nums.index(new_nums[1]) + 1
+            elif target == new_nums[0]:
+                result = nums.index(new_nums[0])
+            elif target < new_nums[1]:
+                result = nums.index(new_nums[1])
+            return result
+            break
+        number = int(len(new_nums)/2)
+        print(number)
+
+def remove_duplicates(nums):
+    '''
+    https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+    Given an integer array nums sorted in non-decreasing order, remove the duplicates
+    in-place such that each unique element appears only once. The relative order of
+    the elements should be kept the same. Then return the number of unique elements in nums.
+    '''
+    nums = list(set(nums))
+    k = len(nums)
+    return k, nums
+
+def remove_element(nums, val):
+    '''
+    https://leetcode.com/problems/remove-element/description/
+    Given an integer array nums and an integer val, remove all
+    occurrences of val in nums in-place. The order of the elements
+    may be changed. Then return the number of elements in nums
+    which are not equal to val.
+    '''
+    for item in nums[:]:
+        if item == val:
+            nums.remove(item)
+    result = len(nums)
+    return result, nums
--- tests/test_easy.py
+++ tests/test_easy.py
@@ -23,7 +23,7 @@ def test_length_last_word_negative(s, expected_result):
     assert result == expected_result
 
 
-@pytest.mark.wip
+# @pytest.mark.wip
 @pytest.mark.parametrize("strs, expected_result",[
         (["flower","flow","flight"], "fl"),
         (["dog","racecar","car"], "")
@@ -60,3 +60,60 @@ def test_merge_two_sorted_lists_positive(list1, list2, expected_result):
 def test_merge_two_sorted_lists_negative(list1, list2, expected_result):
     result = merged_two_sorted_lists(list1, list2)
     assert result != expected_result
+
+
+@pytest.mark.parametrize("nums, target, expected_result", [
+    ([1,3,5,6], 5, 2),
+    ([1,3,5,6], 2, 1),
+    ([1,3,5,6], 7, 4)
+])
+def test_search_insert_positive(nums, target, expected_result):
+    result = search_insert(nums,target)
+    assert result == expected_result
+
+
+@pytest.mark.xfail(reason="Negative")
+@pytest.mark.parametrize("nums, target, expected_result", [
+    ([1,3,5,6], 5, 1),
+    ([1,3,5,6], 2, 2),
+    ([1,3,5,6], 7, 3)
+])
+def test_search_insert_negative(nums, target, expected_result):
+    result = search_insert(nums,target)
+    assert result == expected_result
+
+@pytest.mark.parametrize("nums, len, expected_result", [
+    ([1,1,2], 2 , [1,2]),
+    ([0,0,1,1,1,2,2,3,3,4], 5, [0,1,2,3,4])
+])
+def test_remove_duplicates_positive(nums, len, expected_result):
+    lenght,result = remove_duplicates(nums)
+    assert lenght == len and result == expected_result
+
+@pytest.mark.xfail(reason="Negative")
+@pytest.mark.parametrize("nums, len, expected_result", [
+    ([1,1,2], 2 , [1,3]),
+    ([0,0,1,1,1,2,2,3,3,4], 5, [0,1,2,3,5])
+])
+def test_remove_duplicates_negative(nums, len, expected_result):
+    lenght,result = remove_duplicates(nums)
+    assert lenght == len and result == expected_result
+
+
+@pytest.mark.parametrize("nums, val, expected_val, expected_nums ", [
+    ([3,2,2,3], 3, 2, [2,2]),
+    ([0,1,2,2,3,0,4,2], 2, 5, [0, 1, 3, 0, 4])
+])
+def test_remove_elements_positive(nums, val, expected_val, expected_nums):
+    new_val, new_nums = remove_element(nums,val)
+    assert new_val == expected_val and new_nums == expected_nums
+
+
+@pytest.mark.xfail(reason="Negative")
+@pytest.mark.parametrize("nums, val, expected_val, expected_nums ", [
+    ([3,2,2,3], 3, 2, [2,2]),
+    ([0,1,2,2,3,0,4,2], 2, 5, [0, 1, 3, 0, 4])
+])
+def test_remove_elements_negative(nums, val, expected_val, expected_nums):
+    new_val, new_nums = remove_element(nums,val)
+    assert new_val == expected_val and new_nums == expected_nums
